af3803fd70b6b55d51f139f362056a568e75edbe
Arthur <102627397+shellter01@users.noreply.github.com>
2022-07-03 19:31:23 -0300

    Add files via upload

---
+++ degub.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+echo   " Select the background:"
+echo -e "\033[34m 1 adwaita-day.jpg \033[0m"
+echo -e "\033[34m 2 LightWaves.jpg \033[0m"
+echo -e "\033[34m 3 Road.jpg \033[0m"
+read cmd
+
+if [ $cmd = "1" ];
+then
+	echo -e  "\033[31m Starting debuger...\033[0m"
+	sleep 1
+	gsettings get org.gnome.desktop.background picture-uri 
+	sleep 1
+	gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/gnome/adwaita-day.jpg
+	echo -e "\033[32m Done!\033[0m"
+elif [ $cmd = "2" ];
+then
+	echo -e  "\033[31m Starting debuger...\033[0m"
+        sleep 1
+        gsettings get org.gnome.desktop.background picture-uri 
+        sleep 1
+        gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/gnome/LightWaves.jpg
+        echo -e "\033[32m Done!\033[0m"
+elif [ $cmd = "3" ];
+then
+	echo -e  "\033[31m Starting debuger...\033[0m"
+        sleep 1
+        gsettings get org.gnome.desktop.background picture-uri 
+        sleep 1
+        gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/gnome/Road.jpg
+        echo -e "\033[32m Done!\033[0m"
+
+else
+	exit
+fi
